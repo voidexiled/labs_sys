46e66094221198ba4de8b0e631feb7d57de4012f
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-03-19 12:06:12 -0600

    refactor: added client name and type of client to context menu

--- src/components/laboratory-item.tsx
+++ src/components/laboratory-item.tsx
@@ -7,7 +7,7 @@ import { useClientsTypes } from "@/store/clients_types";
 import { useClients } from "@/store/clients";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useSubjects } from "@/store/subjects";
-import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuSeparator, ContextMenuTrigger } from "./ui/context-menu";
+import { ContextMenu, ContextMenuContent, ContextMenuItem, ContextMenuSeparator, ContextMenuSub, ContextMenuSubContent, ContextMenuSubTrigger, ContextMenuTrigger } from "./ui/context-menu";
 
 
 
@@ -51,28 +51,28 @@ export const LaboratoryItem = (props: {
         <ContextMenu>
             <ContextMenuTrigger>
                 <div
-                    className="relative grid grid-cols-laboratory-item transition-all duration-300 hover:cursor-pointer group min-h-32  "
-                    onClick={() => {
-
-                    }}
+                    className="relative grid grid-cols-laboratory-item transition-all duration-300 hover:cursor-pointer group min-h-32 rounded-md hover:bg-secondary  "
                 >
-                    <div className="py-3 overflow-visible h-full p-4 flex items-center  transition-all ">
+                    <div className="py-3 overflow-visible h-full p-4 flex items-center transition-all">
                         <AspectRatio ratio={16 / 9} className="">
-                            <Image loading="lazy" quality={45} src="/laboratory_cx1.webp" alt="" fill className="drop-shadow-lg shadow-black rounded-md object-cover transition-opacity opacity-0 duration-[2s] "
-                                onLoadingComplete={(image) => { image.classList.remove("opacity-0") }}
+                            <Image loading="lazy" quality={45} src="/laboratory_cx1.webp" alt="" fill className="drop-shadow-lg shadow-black rounded-md object-cover transition-all opacity-0 duration-[2s] group-hover:opacity-100 group-hover:[box-shadow:0_0_12px_1px_rgba(0,0,0,0.08)] "
+                                onLoadingComplete={(image) => {
+                                    image.classList.remove("opacity-0")
+                                    image.classList.add("opacity-80")
+                                }}
                             />
                         </AspectRatio>
                     </div>
 
                     <div className="lab-info py-3 px-5 flex flex-row lg:gap-6 tracking-wider text-sm group-hover:text-foreground/90 transition-all text-pretty group-hover:pl-6">
-                        <div className="flex flex-col basis-[320px] text-muted-foreground font-normal">
-                            <h3 className="text-lg text-foreground group-hover:underline transition-all">{props.lab.label}</h3>
+                        <div className="flex flex-col basis-[320px] text-muted-foreground font-normal text-sm gap-1 ">
+                            <h3 className="text-base text-foreground transition-all hover:font-medium">{props.lab.label}</h3>
                             <span className="">{subject}</span>
 
                             {
                                 props.lab.isBusy
-                                    ? <><span>Ocupado por <span className="">{clientType}</span></span>
-                                        <span className="">{client ? `${client.firstName} ${client.lastName}` : ''}</span>
+                                    ? <><span className="text-primary">Ocupado por <span className="">{clientType}</span></span>
+                                        <span className="decoration-primary text-primary">{client ? `${client.firstName} ${client.lastName}` : ''}</span>
                                     </>
                                     : <span className="text-green-700 font-semibold">Disponible</span>
                             }
@@ -94,8 +94,20 @@ export const LaboratoryItem = (props: {
             <ContextMenuContent>
                 <ContextMenuItem disabled>Viendo {props.lab.label} </ContextMenuItem>
                 {props.lab.isBusy ?
-                    <ContextMenuItem>Desocupar</ContextMenuItem> :
-                    <ContextMenuItem>Ocupar</ContextMenuItem>
+                    <>
+                        <ContextMenuItem disabled>Ocupando {client && `${client.firstName} ${client.lastName}`}  </ContextMenuItem>
+                        <ContextMenuItem disabled>{clientType}</ContextMenuItem>
+                        <ContextMenuItem>Desocupar</ContextMenuItem>
+                    </> :
+                    <ContextMenuSub>
+                        <ContextMenuSubTrigger inset>Ocupar</ContextMenuSubTrigger>
+                        <ContextMenuSubContent>
+                            <ContextMenuItem>Agendar</ContextMenuItem>
+                            <ContextMenuItem>Ahora</ContextMenuItem>
+                        </ContextMenuSubContent>
+
+                    </ContextMenuSub>
+
                 }
                 <ContextMenuSeparator />
                 <ContextMenuItem>Editar</ContextMenuItem>
