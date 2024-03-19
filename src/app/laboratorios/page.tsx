a3fdc1116c2fccb31e934ec259cf454868e07574
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-03-19 12:03:46 -0600

    Refactor: removed fetching data from list

--- src/app/laboratorios/page.tsx
+++ src/app/laboratorios/page.tsx
@@ -4,12 +4,18 @@ import { MainWrapper } from "@/components/main-wrapper";
 import { MainWrapperContent } from "@/components/main-wrapper-content";
 import { MainWrapperHeader } from "@/components/main-wrapper-header";
 import { Retriever } from "@/components/retriever/retriever";
+import { useClients } from "@/store/clients";
+import { useClientsTypes } from "@/store/clients_types";
 import { useLabs } from "@/store/labs";
+import { useSubjects } from "@/store/subjects";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 import { useEffect } from "react";
 
 export default function LaboratoriesPage() {
-    const { setLaboratories } = useLabs();
+    const { laboratories, setLaboratories } = useLabs();
+    const { subjects, setSubjects } = useSubjects();
+    const { clients, setClients } = useClients();
+    const { types, setTypes } = useClientsTypes();
     const supabase = createClientComponentClient();
 
     useEffect(() => {
@@ -35,6 +41,36 @@ export default function LaboratoriesPage() {
 
 
 
+    }, [])
+    useEffect(() => {
+
+        const fetchData = async () => {
+
+            // Fetch Subjects from supabase
+            const { data, error } = await supabase.from("subjects").select("*")
+            if (data) {
+                setSubjects(data);
+            }
+
+            // Fetch Clients from supabase
+            await supabase.from("clients").select("*").then((data) => {
+                if (data.data) {
+                    setClients(data.data);
+                }
+            });
+            // Fetch Client Types from supabase
+            await supabase.from("clients_types").select("*").then((data) => {
+                if (data.data) {
+                    setTypes(data.data);
+                }
+            });
+            console.log("!!!! FETCHING ----")
+            console.log("---Subjects: ", subjects);
+            console.log("---Clients: ", clients);
+            console.log("---Types: ", types);
+            console.log("---- END FETCHING!!!!");
+        }
+        fetchData();
     }, [])
 
     return (
