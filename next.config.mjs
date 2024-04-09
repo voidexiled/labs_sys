3ff0140fbb83ac0f24ad84e12a11be89108b4a78
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-09 05:05:28 -0600

    ci: supabase storage remote pattern

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "gdehlqhfixbmqgpioojy.supabase.co",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
