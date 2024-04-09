b56d14529c367114b9f90d57c39e604f8c5516fc
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-09 05:19:04 -0600

    feat(app): session read and redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+"use server"
 
-export default function Home() {
+import readUserSession from "@/lib/actions";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+
+  const { data: { user } } = await readUserSession();
+
+  if (user) {
+    return redirect("/dashboard/panel");
+  } else {
+    return redirect("/login");
+  }
 
   return (
     <>
