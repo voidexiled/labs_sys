afe14453c841c1cc9f2d5204b5dd93e17e3fb05f
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-10 02:12:55 -0600

    refactor: remove adminClient

--- src/lib/supabase/browser.ts
+++ src/lib/supabase/browser.ts
@@ -2,16 +2,11 @@ import { createBrowserClient } from "@supabase/ssr";
 import { Database } from "../types/supabase";
 
 export function createSupabaseBrowser() {
+  // console.log(process.env.NEXT_PUBLIC_ADMIN_SUPABASE)
   return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ""
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
 
 
-export function createSupabaseBrowserAdmin() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL || "",
-    process.env.SUPABASE_SERVICE_ROLE_KEY || ""
-  );
-}
