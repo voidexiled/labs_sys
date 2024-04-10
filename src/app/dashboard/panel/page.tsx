3c310c8f910ffd887031de87eb0526bb7c734a03
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-10 02:13:47 -0600

    test: add admin role key test

--- src/app/dashboard/panel/page.tsx
+++ src/app/dashboard/panel/page.tsx
@@ -4,6 +4,9 @@ import { MainWrapperContent } from "@/components/main-wrapper-content";
 import { MainWrapperHeader } from "@/components/main-wrapper-header";
 import { redirect } from "next/navigation";
 import readUserSession from "@/lib/actions";
+import createSupabaseServer, { createSupabaseAdmin } from "@/lib/supabase/server";
+import { FormEvent } from "react";
+import axios from "axios";
 // import { BodyPanel } from "@/components/providers/panel/body-test";
 export default async function PanelPage() {
     const { data: { user } } = await readUserSession();
@@ -11,13 +14,17 @@ export default async function PanelPage() {
     if (!user) {
         return redirect("/login");
     }
-
+    const admin = await createSupabaseAdmin();
+    const supabase = await createSupabaseServer();
+    const authUsers = await admin.auth.admin.listUsers();
+    const users = await supabase.from("users_profile").select("*");
+    // console.log("authUsers: ", authUsers.data.users);
+    // console.log("users: ", users.data);
 
 
     return (<MainWrapper>
         <MainWrapperHeader title="Panel de administración" />
         <MainWrapperContent>
-            {/* <BodyPanel /> */}
             <></>
         </MainWrapperContent>
 
