5b352fc323e5fed5a8f6f399ccbd9f677428936e
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-15 02:38:50 -0600

    refactor: scroll component

---
+++ src/components/scroll-area-dashboard.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { ScrollArea } from "./ui/scroll-area";
+import clsx from "clsx";
+
+
+
+export const ScrollAreaDashboard = ({ className, children }: { className?: string, children?: ReactNode }) => {
+    return (
+        <ScrollArea className={clsx("h-[340px] 2xl:h-[600px] pr-8 ", className)}>
+            {children}
+        </ScrollArea>
+    )
+}
