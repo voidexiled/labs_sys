774a05aa794fa4c8c82e40c7c01555417abf33bf
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-17 06:07:52 -0600

    fix: wrapper height

--- src/components/main-wrapper-content.tsx
+++ src/components/main-wrapper-content.tsx
@@ -1,7 +1,7 @@
 
 export const MainWrapperContent = (props: { children: React.ReactNode }) => {
     return (
-        <div className="content py-2 px-16 pb-8">
+        <div className="content py-2 px-16">
             {props.children}
         </div>
     )
--- src/components/scroll-area-dashboard.tsx
+++ src/components/scroll-area-dashboard.tsx
@@ -6,7 +6,7 @@ import clsx from "clsx";
 
 export const ScrollAreaDashboard = ({ className, children }: { className?: string, children?: ReactNode }) => {
     return (
-        <ScrollArea className={clsx("h-[340px] 2xl:h-[630px] pr-8 ", className)}>
+        <ScrollArea className={clsx("h-[340px] 2xl:h-[615px] pr-8 ", className)}>
             {children}
         </ScrollArea>
     )
