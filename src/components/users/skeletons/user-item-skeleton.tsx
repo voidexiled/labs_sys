c54f886a38326bed575e18f441375fb1975cbbe1
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-20 18:09:42 -0600

    fix: user skeleton

--- src/components/users/skeletons/user-item-skeleton.tsx
+++ src/components/users/skeletons/user-item-skeleton.tsx
@@ -15,7 +15,7 @@ export function UserItemSkeleton() {
         </div>
         <div className="py-4 px-5 flex flex-col justify-between tracking-wider text-sm text-muted-foreground transition-all text-pretty">
             <span className="text-foreground group-hover:font-medium transition-all">
-                <Skeleton className="w-[240px] h-3 rounded-full " />
+                <Skeleton className="w-[230px] h-3 rounded-full " />
             </span>
             <div className="flex flex-row gap-3 text-sm transition-all group-hover:pl-2 group-hover:text-foreground">
                 <span><Skeleton className="w-[100px] h-3 rounded-full " /></span>
