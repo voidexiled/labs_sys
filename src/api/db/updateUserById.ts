8eb59b5914df4b7f431fd2f88a9c6d9f15a3df95
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-04-22 06:44:54 -0600

    refactor: database schema updated

--- src/api/db/updateUserById.ts
+++ src/api/db/updateUserById.ts
@@ -7,5 +7,5 @@ import { formatDate } from '@/lib/utils';
 export default async function updateUserById({id}: {id: string}) {
     const supabase = createSupabaseBrowser();
     const updatedAt = formatDate(new Date());
-    return await supabase.from('users_profile').update({updated_at: updatedAt}).eq('id', id).select('*');
+    return await supabase.from("users").update({updated_at: updatedAt}).eq('id', id).select('*');
 }
